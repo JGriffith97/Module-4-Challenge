2d534a873d126f8fa2a22ed612690a729248e1b4
Jayden Griffith <griffithjayden97@outlook.com>
2022-05-09 18:21:00 -0600

    Added pseudo-coding to establish layout, ideas

---
+++ script.js
@@ -0,0 +1,9 @@
+// Query selector to access button and timer elements
+// Query selector to access quiz area to change the quiz content
+
+// variable declaring the timer
+// variable declaring the timer count
+
+// Selectors, potentially, to access CSS content?
+
+// conditions for quiz; correct, wrong, if, if else, etc.
