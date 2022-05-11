bd9f70c23154d8b683c559fb30464a29233709e6
Jayden Griffith <griffithjayden97@outlook.com>
2022-05-11 13:41:36 -0600

    Experimented with various for-loops. Trying to create a system where the quiz creates 4 li elements at once, since I have to do it many times.

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 // Query selector to access button and timer elements
-var startBtn = document.querySelector(".start");
-var timerEl = document.querySelector(".timer");
+var startBtn = document.querySelector("#start");
+var timerEl = document.querySelector("#timer");
 var quizHeadEl = document.querySelector("#quizHead");
 var listEl = document.querySelector("#list");
-// var addLi = document.createElement("li");
+var li = document.querySelector("#li");
+var main = document.querySelector("main");
+var container = document.querySelector("#quizContainer");
+
+var questions1 = ["test", "test2", "test3", "test4"]
 
 // Query selector to access quiz area to change the quiz content
 // element(via var).appendChild(variable or element to be created)
 
+var quizStart = function() {
+  var addLi = document.createElement("li");
+  for (let i = 0; i < questions1.length; i++) {
+    listEl.addLi;
+  }
+  listEl[0].textContent = questions1[0]
+  listEl[1].textContent = questions1[1]
+  listEl[2].textContent = questions1[2]
+  listEl[3].textContent = questions1[3]
+}
 // variable declaring the timer
 // variable declaring the timer count
 
@@ -16,7 +30,10 @@ var listEl = document.querySelector("#list");
 // conditions for quiz; correct, wrong, if, if else, etc.
 
 // When botton is clicked, hide button, allow quiz questions to fill the space
-startBtn.addEventListener("click", //function)
+startBtn.addEventListener("click", function() {
+  quizStart();
+  startBtn.remove();
+});
 
 
 // Example code for adding elements
