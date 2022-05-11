cc4b7a028d42ca3c6bc676e6e9f6bba66dead3be
Jayden Griffith <griffithjayden97@outlook.com>
2022-05-11 11:32:46 -0600

    Added query selectors for eventual manipulation of the DOM, as well as an event listener for the start quiz button

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 // Query selector to access button and timer elements
+var startBtn = document.querySelector(".start");
+var timerEl = document.querySelector(".timer");
+var quizHeadEl = document.querySelector("#quizHead");
+var listEl = document.querySelector("#list");
+// var addLi = document.createElement("li");
+
 // Query selector to access quiz area to change the quiz content
+// element(via var).appendChild(variable or element to be created)
 
 // variable declaring the timer
 // variable declaring the timer count
@@ -9,3 +16,22 @@
 // conditions for quiz; correct, wrong, if, if else, etc.
 
 // When botton is clicked, hide button, allow quiz questions to fill the space
+startBtn.addEventListener("click", //function)
+
+
+// Example code for adding elements
+/* <html>
+<body>
+<div id="new">
+<p id="p1">Tutorix</p>
+<p id="p2">Tutorialspoint</p>
+</div>
+<script>
+   var tag = document.createElement("p");
+   var text = document.createTextNode("Tutorix is the best e-learning platform");
+   tag.appendChild(text);
+   var element = document.getElementById("new");
+   element.appendChild(tag);
+</script>
+</body>
+</html> */
